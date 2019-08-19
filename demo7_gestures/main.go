08b7a22e580a81766549bb57b82b1ce493cb7d2b
Gergely Brautigam <gergely.brautigam@acquia.com>
2019-08-19 09:54:17 +0200

    Gestures is now sort of working.

--- demo7_gestures/main.go
+++ demo7_gestures/main.go
@@ -6,44 +6,130 @@
 //
 // 		go run ./cmd/hand-gestures/main.go 0
 //
-// +build example
-
 package main
 
 import (
 	"fmt"
 	"image"
 	"image/color"
+	"io"
 	"math"
-	"os"
+	"os/exec"
+	"strconv"
+	"sync/atomic"
+	"time"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/platforms/dji/tello"
+	"gobot.io/x/gobot/platforms/joystick"
 
 	"gocv.io/x/gocv"
 )
 
-const MinimumArea = 3000
+type pair struct {
+	x float64
+	y float64
+}
+
+const (
+	frameX    = 400
+	frameY    = 300
+	frameSize = frameX * frameY * 3
+	offset    = 32767.0
+)
+
+var (
+	// ffmpeg command to decode video stream from drone
+	ffmpeg = exec.Command("ffmpeg", "-hwaccel", "auto", "-hwaccel_device", "opencl", "-i", "pipe:0",
+		"-pix_fmt", "bgr24", "-s", strconv.Itoa(frameX)+"x"+strconv.Itoa(frameY), "-f", "rawvideo", "pipe:1")
+	ffmpegIn, _  = ffmpeg.StdinPipe()
+	ffmpegOut, _ = ffmpeg.StdoutPipe()
+
+	// gocv
+	window = gocv.NewWindow("Tello")
+
+	// tracking
+	tracking = false
+
+	// drone
+	drone      = tello.NewDriver("8890")
+	flightData *tello.FlightData
+
+	// joystick
+	joyAdaptor                   = joystick.NewAdaptor()
+	stick                        = joystick.NewDriver(joyAdaptor, "dualshock4")
+	leftX, leftY, rightX, rightY atomic.Value
+)
+
+func init() {
+	leftX.Store(0.0)
+	leftY.Store(0.0)
+	rightX.Store(0.0)
+	rightY.Store(0.0)
+
+	// process drone events in separate goroutine for concurrency
+	go func() {
+		handleJoystick()
+
+		if err := ffmpeg.Start(); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		_ = drone.On(tello.FlightDataEvent, func(data interface{}) {
+			// TODO: protect flight data from race condition
+			flightData = data.(*tello.FlightData)
+		})
+
+		_ = drone.On(tello.ConnectedEvent, func(data interface{}) {
+			fmt.Println("Connected")
+			_ = drone.StartVideo()
+			_ = drone.SetVideoEncoderRate(tello.VideoBitRateAuto)
+			_ = drone.SetExposure(0)
+			gobot.Every(30*time.Millisecond, func() {
+				_ = drone.StartVideo()
+			})
+		})
+
+		_ = drone.On(tello.VideoFrameEvent, func(data interface{}) {
+			pkt := data.([]byte)
+			if _, err := ffmpegIn.Write(pkt); err != nil {
+				fmt.Println(err)
+			}
+		})
+
+		robot := gobot.NewRobot("tello",
+			[]gobot.Connection{joyAdaptor},
+			[]gobot.Device{drone, stick},
+		)
+
+		_ = robot.Start()
+	}()
+}
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("How to run:\n\thand-gestures [camera ID]")
-		return
+	for {
+		// get next frame from stream
+		buf := make([]byte, frameSize)
+		if _, err := io.ReadFull(ffmpegOut, buf); err != nil {
+			fmt.Println(err)
+			continue
+		}
+		img, _ := gocv.NewMatFromBytes(frameY, frameX, gocv.MatTypeCV8UC3, buf)
+		if img.Empty() {
+			continue
+		}
+
+		detectGesture(&img)
+		////
+		window.IMShow(img)
+		if window.WaitKey(10) >= 0 {
+			break
+		}
 	}
+}
 
-	// parse args
-	deviceID := os.Args[1]
-
-	webcam, err := gocv.OpenVideoCapture(deviceID)
-	if err != nil {
-		fmt.Printf("Error opening video capture device: %v\n", deviceID)
-		return
-	}
-	defer webcam.Close()
-
-	window := gocv.NewWindow("Hand Gestures")
-	defer window.Close()
-
-	img := gocv.NewMat()
-	defer img.Close()
-
+func detectGesture(img *gocv.Mat) {
 	imgGrey := gocv.NewMat()
 	defer imgGrey.Close()
 
@@ -59,63 +145,48 @@ func main() {
 	defects := gocv.NewMat()
 	defer defects.Close()
 
-	green := color.RGBA{0, 255, 0, 0}
+	green := color.RGBA{R: 0, G: 255, B: 0, A: 0}
 
-	fmt.Printf("Start reading device: %v\n", deviceID)
-	for {
-		if ok := webcam.Read(&img); !ok {
-			fmt.Printf("Device closed: %v\n", deviceID)
-			return
-		}
-		if img.Empty() {
-			continue
-		}
+	// cleaning up image
+	gocv.CvtColor(*img, &imgGrey, gocv.ColorBGRToGray)
+	gocv.GaussianBlur(imgGrey, &imgBlur, image.Pt(35, 35), 0, 0, gocv.BorderDefault)
+	gocv.Threshold(imgBlur, &imgThresh, 0, 255, gocv.ThresholdBinaryInv+gocv.ThresholdOtsu)
 
-		// cleaning up image
-		gocv.CvtColor(img, &imgGrey, gocv.ColorBGRToGray)
-		gocv.GaussianBlur(imgGrey, &imgBlur, image.Pt(35, 35), 0, 0, gocv.BorderDefault)
-		gocv.Threshold(imgBlur, &imgThresh, 0, 255, gocv.ThresholdBinaryInv+gocv.ThresholdOtsu)
+	// now find biggest contour
+	contours := gocv.FindContours(imgThresh, gocv.RetrievalExternal, gocv.ChainApproxSimple)
+	c := getBiggestContour(contours)
 
-		// now find biggest contour
-		contours := gocv.FindContours(imgThresh, gocv.RetrievalExternal, gocv.ChainApproxSimple)
-		c := getBiggestContour(contours)
+	gocv.ConvexHull(c, &hull, true, false)
+	gocv.ConvexityDefects(c, hull, &defects)
 
-		gocv.ConvexHull(c, &hull, true, false)
-		gocv.ConvexityDefects(c, hull, &defects)
+	var angle float64
+	defectCount := 0
+	for i := 0; i < defects.Rows(); i++ {
+		start := c[defects.GetIntAt(i, 0)]
+		end := c[defects.GetIntAt(i, 1)]
+		far := c[defects.GetIntAt(i, 2)]
 
-		var angle float64
-		defectCount := 0
-		for i := 0; i < defects.Rows(); i++ {
-			start := c[defects.GetIntAt(i, 0)]
-			end := c[defects.GetIntAt(i, 1)]
-			far := c[defects.GetIntAt(i, 2)]
+		a := math.Sqrt(math.Pow(float64(end.X-start.X), 2) + math.Pow(float64(end.Y-start.Y), 2))
+		b := math.Sqrt(math.Pow(float64(far.X-start.X), 2) + math.Pow(float64(far.Y-start.Y), 2))
+		c := math.Sqrt(math.Pow(float64(end.X-far.X), 2) + math.Pow(float64(end.Y-far.Y), 2))
 
-			a := math.Sqrt(math.Pow(float64(end.X-start.X), 2) + math.Pow(float64(end.Y-start.Y), 2))
-			b := math.Sqrt(math.Pow(float64(far.X-start.X), 2) + math.Pow(float64(far.Y-start.Y), 2))
-			c := math.Sqrt(math.Pow(float64(end.X-far.X), 2) + math.Pow(float64(end.Y-far.Y), 2))
+		// apply cosine rule here
+		angle = math.Acos((math.Pow(b, 2)+math.Pow(c, 2)-math.Pow(a, 2))/(2*b*c)) * 57
 
-			// apply cosine rule here
-			angle = math.Acos((math.Pow(b, 2)+math.Pow(c, 2)-math.Pow(a, 2))/(2*b*c)) * 57
-
-			// ignore angles > 90 and highlight rest with dots
-			if angle <= 90 {
-				defectCount++
-				gocv.Circle(&img, far, 1, green, 2)
-			}
-		}
-
-		status := fmt.Sprintf("defectCount: %d", defectCount+1)
-
-		rect := gocv.BoundingRect(c)
-		gocv.Rectangle(&img, rect, color.RGBA{255, 255, 255, 0}, 2)
-
-		gocv.PutText(&img, status, image.Pt(10, 20), gocv.FontHersheyPlain, 1.2, green, 2)
-
-		window.IMShow(img)
-		if window.WaitKey(1) == 27 {
-			break
+		// ignore angles > 90 and highlight rest with dots
+		if angle <= 90 {
+			defectCount++
+			gocv.Circle(img, far, 1, green, 2)
 		}
 	}
+
+	status := fmt.Sprintf("defectCount: %d", defectCount+1)
+
+	rect := gocv.BoundingRect(c)
+	gocv.Rectangle(img, rect, color.RGBA{R: 255, G: 255, B: 255, A: 0}, 2)
+
+	gocv.PutText(img, status, image.Pt(10, 20), gocv.FontHersheyPlain, 1.2, green, 2)
+
 }
 
 func getBiggestContour(contours [][]image.Point) []image.Point {
@@ -129,4 +200,99 @@ func getBiggestContour(contours [][]image.Point) []image.Point {
 		}
 	}
 	return contours[index]
+}
+
+func handleJoystick() {
+	stick.On(joystick.CirclePress, func(data interface{}) {
+		drone.Forward(0)
+		drone.Up(0)
+		drone.Clockwise(0)
+		tracking = !tracking
+	})
+	stick.On(joystick.SquarePress, func(data interface{}) {
+		fmt.Println("battery:", flightData.BatteryPercentage)
+	})
+	stick.On(joystick.TrianglePress, func(data interface{}) {
+		drone.TakeOff()
+		println("Takeoff")
+	})
+	stick.On(joystick.XPress, func(data interface{}) {
+		drone.Land()
+		println("Land")
+	})
+	stick.On(joystick.LeftX, func(data interface{}) {
+		val := float64(data.(int16))
+		leftX.Store(val)
+	})
+
+	stick.On(joystick.LeftY, func(data interface{}) {
+		val := float64(data.(int16))
+		leftY.Store(val)
+	})
+
+	stick.On(joystick.RightX, func(data interface{}) {
+		val := float64(data.(int16))
+		rightX.Store(val)
+	})
+
+	stick.On(joystick.RightY, func(data interface{}) {
+		val := float64(data.(int16))
+		rightY.Store(val)
+	})
+	gobot.Every(50*time.Millisecond, func() {
+		rightStick := getRightStick()
+
+		switch {
+		case rightStick.x < -10:
+			drone.Forward(tello.ValidatePitch(rightStick.x, offset))
+		case rightStick.x > 10:
+			drone.Backward(tello.ValidatePitch(rightStick.x, offset))
+		default:
+			drone.Forward(0)
+		}
+
+		switch {
+		case rightStick.y > 10:
+			drone.Right(tello.ValidatePitch(rightStick.y, offset))
+		case rightStick.y < -10:
+			drone.Left(tello.ValidatePitch(rightStick.y, offset))
+		default:
+			drone.Right(0)
+		}
+	})
+
+	gobot.Every(50*time.Millisecond, func() {
+		leftStick := getLeftStick()
+		switch {
+		case leftStick.y < -10:
+			drone.Up(tello.ValidatePitch(leftStick.y, offset))
+		case leftStick.y > 10:
+			drone.Down(tello.ValidatePitch(leftStick.y, offset))
+		default:
+			drone.Up(0)
+		}
+
+		switch {
+		case leftStick.x > 20:
+			drone.Clockwise(tello.ValidatePitch(leftStick.x, offset))
+		case leftStick.x < -20:
+			drone.CounterClockwise(tello.ValidatePitch(leftStick.x, offset))
+		default:
+			drone.Clockwise(0)
+		}
+	})
+}
+
+func getLeftStick() pair {
+	s := pair{x: 0, y: 0}
+	s.x = leftX.Load().(float64)
+	s.y = leftY.Load().(float64)
+	return s
+}
+
+func getRightStick() pair {
+	s := pair{x: 0, y: 0}
+	s.x = rightX.Load().(float64)
+	s.y = rightY.Load().(float64)
+	return s
 }
